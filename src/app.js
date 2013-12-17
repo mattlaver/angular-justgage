1264b6f1d7d404d353a182ecd8e63c491fea944d
Martin Feckie <mfeckie@gmail.com>
2013-12-17 14:05:00 +0800

    Add controller values for demo

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
-angular.module('app', ['ngJustGage']).controller('myController', function($scope) {});
+angular.module('app', ['ngJustGage']).controller('myController', function($scope) {
+  $scope.value1 = 1;
+});
