e9d4327b86a066beceff6f06901437356d30b3d4
Matt Laver <mattlaver@gmail.com>
2014-03-24 21:58:57 +0000

    Added bower support

---
+++ bower.js
@@ -0,0 +1,41 @@
+{
+    "name": "angular-justgage",
+    "description": "JustGage directive for AngularJS",
+    "version": "1.0",
+    "keywords": [
+    "angular",
+    "justgage"
+],
+    "homepage": "https://github.com/mattlaver/angular-justgage",
+    "bugs": "https://github.com/mattlaver/angular-justgage/issues",
+    "author": {
+    "name": "Matt Laver",
+        "email": "mattlaver@gmail.com",
+        "url": "https://github.com/mattlaver"
+},
+    "repository": {
+    "type": "git",
+        "url": "https://github.com/mattlaver/angular-justgage.git"
+},
+    "licenses": [
+    {
+        "type": "MIT"
+    }
+],
+    "main": [
+    "src/ng-justgage.js",
+],
+    "ignore": [
+    "demo",
+    "libs",
+    "screen.png",
+    "README.md",
+    "src/app.js"
+],
+    "dependencies": {
+    "angular": "~1.2.10",
+    "JustGage": "~1.0.1"
+},
+    "devDependencies": {
+}
+}
