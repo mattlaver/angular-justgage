e8b4ac980fbf5e256235318697bee254925049a9
Vinícius Krolow <krolow@gmail.com>
2014-07-15 01:34:22 -0300

    Fixing indentation and missing commas

--- ng-justgage.js
+++ ng-justgage.js
@@ -1,33 +1,32 @@
-
 angular.module("ngJustGage", [])
-    .directive('justGage', ['$timeout', function ($timeout) {
-      return {
-        restrict: 'EA',
-        scope: {
-          id: '@',
-          class: '@',
-          min: '=',
-          max: '=',
-          title: '@',
-          value: '='
-        },
-        template: '<div id="{{id}}-justgage" class="{{class}}"></div>',
-        link: function (scope) {
-          $timeout(function () {
-            var g = new JustGage({
-              id: scope.id + '-justgage',
-              min: scope.min,
-              max: scope.max,
-              title: scope.title,
-              value: scope.value
-            })
-
-            scope.$watch('value', function (updatedValue) {
-              if (updatedValue) {
-                g.refresh(updatedValue)
-              }
-            }, true);
+  .directive('justGage', ['$timeout', function ($timeout) {
+    return {
+      restrict: 'EA',
+      scope: {
+        id: '@',
+        class: '@',
+        min: '=',
+        max: '=',
+        title: '@',
+        value: '='
+      },
+      template: '<div id="{{id}}-justgage" class="{{class}}"></div>',
+      link: function (scope) {
+        $timeout(function () {
+          var graph = new JustGage({
+            id: scope.id + '-justgage',
+            min: scope.min,
+            max: scope.max,
+            title: scope.title,
+            value: scope.value
           });
-        }
-      };
-    }]);
+
+          scope.$watch('value', function (updatedValue) {
+            if (updatedValue) {
+              graph.refresh(updatedValue);
+            }
+          }, true);
+        });
+      }
+    };
+  }]);
