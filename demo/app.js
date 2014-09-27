57141c9c73e4f79e90f663e3dbdf885fdae9b18e
Matt <mattlaver@gmail.com>
2014-09-27 20:04:14 +0100

    Added demos for new options feature

--- demo/app.js
+++ demo/app.js
@@ -1,9 +1,10 @@
 angular.module('app', ['ngJustGage']).controller('myController', function($scope) {
     $scope.value1 = 42;
-
+    $scope.value2 = 42;
     setInterval(function(){
         $scope.$apply(function() {
             $scope.value1 = getRandomInt(10, 90);
+			$scope.value2 = getRandomInt(10, 90);
         });
     }, 1000);
 });
